acb62a06897900b537f663c75d412343271eba8b
kanityi <kanityi@outlook.com>
2020-08-18 15:55:29 +0200

    finilising ft_ls projest

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smadesi <smadesi@student.wethinkcode.co    +#+  +:+       +#+        */
+/*   By: smadesi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 10:55:02 by smadesi           #+#    #+#             */
 /*   Updated: 2020/03/11 09:40:59 by smadesi          ###   ########.fr       */
@@ -13,16 +13,19 @@
 #include "../includes/ft_ls.h"
 #include "../libft/includes/libft.h"
 
-void ls_processer(char **s, t_ls_flags *flags) {
+void ls_processer(char **s, t_ls_flags *flags)
+{
     int i;
     int count;
 
     i = 0;
     count = 0;
-    while (s[++i]) {
+    while (s[++i])
+    {
         if (s[i][0] == '-' && ft_isprint(s[i][1]))
             continue;
-        else {
+        else
+        {
             path_handler(s[i], flags);
             count++;
         }
@@ -32,14 +35,18 @@ void ls_processer(char **s, t_ls_flags *flags) {
 }
 
 /** ft_ls  equvalant to ls command*/
-void ft_ls(char **str) {
+
+void ft_ls(char **str)
+{
     t_ls_flags flags;
     int i;
 
     i = 0;
     init_flags(&flags);
-    while (str[++i]) {
-        if (str[i][0] == '-' && ft_isprint(str[i][1])) {
+    while (str[++i])
+    {
+        if (str[i][0] == '-' && ft_isprint(str[i][1]))
+        {
             if (!validate_flags(str[i], &flags))
                 exit(EXIT_FAILURE);
         }
@@ -47,13 +54,14 @@ void ft_ls(char **str) {
     ls_processer(str, &flags);
 }
 
-int main(int argc, char **argv){    
+int main(int argc, char **argv)
+{
     // int i = 0;
     (void)argc;
     t_ls_flags *data;
     if (!(data = (t_ls_flags *)malloc(sizeof(t_ls_flags))))
         return 0;
     ft_ls(argv);
-    
+
     return 0;
 }
